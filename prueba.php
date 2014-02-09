0c45c85d1179e2e29ef97b5db73bbbb63271a7e3
joseluis laso <wld1373@gmail.com>
2014-02-09 09:56:32 +0100

    - Este es mi primer commit

---
+++ prueba.php
@@ -0,0 +1,15 @@
+<?php
+
+// ejemplo para ilustrar los conflictos en GIT y su resolucion
+
+print "este es un sencillo ejemplo que permite ilustrar los conlfictos en git" . PHP_EOL;
+
+$a = 1;
+$b = 2;
+
+print "la suma de a y b es " . ($a + $b) . PHP_EOL;
+
+print "este es un ejemplo muy sencillo" . PHP_EOL;
+
+
+
